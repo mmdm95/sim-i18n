c1db1cc00841b61bc51a69acd3eaa11b65354c6f
MMDM <mmdehghan95@gmail.com>
2020-10-06 04:08:41 +0330

    simple autoloader tested

--- src/Tests/config.php
+++ src/Tests/config.php
@@ -4,6 +4,7 @@ use Sim\I18n\ISOLanguageCodes;
 use Sim\I18n\Translate;
 
 include_once '../../vendor/autoload.php';
+//include_once '../../autoloader.php';
 
 $translate = new Translate();
 //$translate->createLanguageFile(ISOLanguageCodes::LANGUAGE_FRENCH, __DIR__ . DIRECTORY_SEPARATOR . 'languages');
